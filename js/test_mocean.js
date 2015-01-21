ad766ecaeee2b92cc5185baa3618e40b1c5b3986
Rory Petty <rorypetty@gmail.com>
2015-01-21 18:26:39 -0500

    debugging cap errors

---
+++ js/test_mocean.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+	console.log("hi");
+  	window._ads2 = window._ads2 || [];
+	window._ads2.push({
+    	"url": "http://ads.moceanads.com/ad?site=119325&zone=343717&type=-1",
+    	"containerId": 'ads2_container_343717'
+	});
+}
+
