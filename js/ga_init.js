838b0af3078bf23105c23a6faed87f625e218274
Rory Petty <rorypetty@gmail.com>
2015-02-19 16:51:49 -0800

    flurry integration test

--- js/ga_init.js
+++
@@ -1,3 +0,0 @@
-console.log('rory');
-ga('create', 'UA-54046308-1', 'none');
-ga('send', 'pageview');
--- js/test_flurry.js
+++ js/test_flurry.js
@@ -1,11 +1,13 @@
-// window.onload=function(){
-// 	console.log("hi");
-//   	window._ads2 = window._ads2 || [];
-// 	window._ads2.push({
-//     	"url": "http://ads.moceanads.com/ad?site=119325&zone=343717&type=-1",
-//     	"containerId": 'ads2_container_343717'
-// 	});
-// }
+FlurryAgent.startSession("KDWDFV7VDVDPVMQMS89X");
+
+FlurryAgent.setUserId(1);
+console.log("foo");
+FlurryAgent.logEvent("foo");
+
+
+
+
+
 
 function getXHR(url, callback) {
     var xhr = new XMLHttpRequest();       // Creates a new XHR request
@@ -37,12 +39,3 @@ function getXHR(url, callback) {
  
     xhr.send();                          // Makes the request
 }
-
-var mocean_response = ""
-var test_url = "http://ads.moceanads.com/ad?site=119325&zone=343717&type=-1&ip=173.3.77.115&ua=Mozilla%2F5.0+%28Windows%3B+U%3B+Windows+NT+5.2%3B+ru%3B+rv%3A1.9.0.19%29+Gecko%2F2010031422+Firefox%2F3.0.19+%28.NET+CLR+3.5.30729%29&key=6&jsvar=mocean_response&type=3"
-
-getXHR(test_url, function(data){
-	console.log("response:")
-	console.log(data);
-	console.log(mocean_response);
-});
