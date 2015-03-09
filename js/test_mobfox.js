bc257046354275e34683aed8682fea932521ec54
Rory Petty <rorypetty@gmail.com>
2015-03-09 18:49:58 -0400

    testing mobfox

---
+++ js/test_mobfox.js
@@ -0,0 +1,12 @@
+RequestAd_({
+
+    // Ad Request Variables
+    s:'86ff3c2bd73412bd66e076e6a3b506da',
+    m:'live', <!-- Change to 'test' to show a test ad -->
+    adspace_width:'320', <!-- Maximum Width of your Banner -->
+    adspace_height:'50', <!-- Maximum Height of your Banner -->
+    adspace_strict:'0', <!-- If you change this parameter to 1, the ad size set above will be strict and no smaller ads will be shown. -->
+    backfillhtml:'<p>noooo ad</p>', <!-- Insert HTML Code here that should be shown when no ad is available -->
+    prependclickcontent:'', <!-- Prepend something before Click URLs. It is recommended to leave this parameter empty.  -->
+    trackingpixelurl:'' <!-- Ping a Tracking Pixel everytime an ad is shown. -->
+});
